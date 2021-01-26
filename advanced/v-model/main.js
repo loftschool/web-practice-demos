a34f07bd1f5acefd56d6956a97db7a3eca583d05
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-01-26 14:34:21 +0300

    подготовил тему по v-model

--- advanced/v-model/main.js
+++ advanced/v-model/main.js
@@ -1,52 +1,17 @@
 const inputComp = {
   template: "#input-comp",
-  props: ['value'],
-  modal: {
-    prop: "value",
-    event: "input"
-  }
 };
 
 const checkbox = {
   template: "#checkbox",
-  props: ['value'],
-  model: {
-    prop: "value",
-    event: "change"
-  }
 };
 
 const group = {
   template: "#group",
-  props: ["items"],
-  model: {
-    prop: "items",
-    event: "itemsChanged"
-  },
-  data() {
-    return {
-      inner: this.items
-    }
-  },
-  methods: {
-    expose() {
-      this.$emit("itemsChanged", this.inner);
-    }
-  }
 }
 
 const radio = {
   template: "#radio",
-  props: ["value"],
-  model: {
-    prop: "value",
-    event: "change"
-  },
-  methods: {
-    handleChange(event) {
-      this.$emit('change', event.target.value)
-    }
-  }
 }
 
 const app = new Vue({
