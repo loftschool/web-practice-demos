fad34cc3628d7cdd109f07e91fb6244de639793c
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2021-01-26 21:08:56 +0300

    v-model:webinar

--- advanced/v-model/main.js
+++ advanced/v-model/main.js
@@ -1,17 +1,57 @@
 const inputComp = {
   template: "#input-comp",
+  props: ['value'],
+  model: {
+    prop: "value",
+    event: "nonInput"
+  }
 };
 
 const checkbox = {
   template: "#checkbox",
+  props: ['value'],
+  model: {
+    prop: "value",
+    event: "change"
+  }
 };
 
 const group = {
   template: "#group",
+  props: ['items'],
+  data(){
+    return {
+      inner: this.items
+    }
+  },
+  model: {
+    prop: "items",
+    event: "itemsChange"
+  },
+  methods: {
+    handleChange() {
+      this.$emit("itemsChange", this.inner);
+    }
+  }
 }
 
 const radio = {
   template: "#radio",
+  props: ['value'],
+  model: {
+    prop: "value",
+    event: "change"
+  },
+  data() {
+    return {
+      controls: ['visa', 'master', 'mir']
+    }
+  },
+  methods: {
+    handleChange(event) {
+      this.$emit("change", event.target.value)
+    }
+  }
 }
 
 const app = new Vue({
@@ -22,8 +62,8 @@ const app = new Vue({
     return {
       myText: "test",
       yesNo: true,
-      group: ['green'],
-      radio: "master"
+      group: ['green', 'red'],
+      radio: "visa"
     }
   }
 });
